93bbfb97c63ec7521e485dcb4369cca765f884e3
gowthambalajinagarajan <61041069+gowthambalajinagarajan@users.noreply.github.com>
2020-04-26 11:06:13 +0530

    Update SwitchWindow.java

--- src/Selenium/SwitchWindow.java
+++ src/Selenium/SwitchWindow.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class SwitchWindow {
+public class SwitchWindow 123 {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\acer\\Desktop\\Java\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
