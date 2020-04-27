ca1ff91eea042184aff4ad3e6bc87b21fdad7aad
gowthambalajinagarajan <gowthambalaji18@gmail.com>
2020-04-27 11:47:44 +0530

    RadioButton

---
+++ src/Selenium/RadioButton.java
@@ -0,0 +1,20 @@
+package Selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.List;
+
+public class RadioButton {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\acer\\Desktop\\Java\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.keynotesupport.com/internet/web-contact-form-example-radio-buttons.shtml");
+		
+		List<WebElement> radio = driver.findElements(By.xpath("//input[@type='radio']"));
+		for(WebElement el : radio){
+		el.click();
+		}
+}
+}
